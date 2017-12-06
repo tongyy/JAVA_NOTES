2fb9d63a860929939a6ead05a0943f5e03b8d4a5
Tony ZL YANG <tony0x00@gmail.com>
2017-12-06 23:38:10 +0800

    fix space

--- src/main/java/com/tongyy/encode/EncodeByteDemo.java
+++ src/main/java/com/tongyy/encode/EncodeByteDemo.java
@@ -23,12 +23,12 @@ public class EncodeByteDemo {
 	
 	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
 	private static String bytesToHex(byte[] bytes) {
-	    char[] hexChars = new char[bytes.length * 2];
-	    for ( int j = 0; j < bytes.length; j++ ) {
-	        int v = bytes[j] & 0xFF;
-	        hexChars[j * 2] = hexArray[v >>> 4];
-	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-	    }
+		char[] hexChars = new char[bytes.length * 2];
+		for ( int j = 0; j < bytes.length; j++ ) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
 	    return new String(hexChars);
 	}
 }
